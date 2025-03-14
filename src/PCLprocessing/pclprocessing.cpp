1fa1b823100e55ef8d093c1eec79caae1849ffa6
IgnorAnsel <ansel09@163.com>
2025-03-15 00:25:12 +0800

    没啥说的

---
+++ src/PCLprocessing/pclprocessing.cpp
@@ -0,0 +1,7 @@
+#include "pclprocessing/pclprocessing.h"
+
+// pc => pclprocessing
+namespace pc {
+    PCLProcessing::PCLProcessing() {}
+    PCLProcessing::~PCLProcessing() {}
+}
---
+++ src/Test/Test_pclprocessing.cpp
@@ -0,0 +1,8 @@
+#include "pclprocessing/pclprocessing.h"
+
+int main()
+{
+    pc::PCLProcessing pclprocess;
+    
+    return 0;
+}
